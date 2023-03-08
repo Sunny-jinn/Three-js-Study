3e1fbe33805220d0b81068f2df21ae63c54e490d
Sunny-jinn <rlawlsdn316@gmail.com>
2023-03-08 14:05:23 +0900

    feat: Add box and sphere

---
+++ src/App.js
@@ -0,0 +1,90 @@
+import React, { Suspense, useRef } from "react";
+import "./App.css";
+import { Canvas } from "@react-three/fiber";
+import Box from "./Box";
+import styled from "styled-components";
+
+import { useGLTF, OrbitControls } from "@react-three/drei";
+import AnimatedSphere from "./AnimatedSphere";
+
+const Wrapper = styled.div`
+  position: relative;
+
+  canvas {
+    height: 500px;
+  }
+`;
+function Model(props) {
+  const { nodes, materials } = useGLTF("../scene.gltf");
+  return (
+    <group {...props} dispose={null} scale={0.5}>
+      <group rotation={[-Math.PI / 2, 0, 0]}>
+        <mesh
+          geometry={nodes.Object_3.geometry}
+          material={materials.material_7}
+        />
+        <mesh
+          geometry={nodes.Object_5.geometry}
+          material={materials.material_6}
+        />
+        <mesh
+          geometry={nodes.Object_7.geometry}
+          material={materials.material_5}
+        />
+        <mesh
+          geometry={nodes.Object_9.geometry}
+          material={materials.material_4}
+        />
+        <mesh
+          geometry={nodes.Object_11.geometry}
+          material={materials.material_3}
+        />
+        <mesh
+          geometry={nodes.Object_13.geometry}
+          material={materials.material_2}
+        />
+        <mesh
+          geometry={nodes.Object_15.geometry}
+          material={materials.material_1}
+        />
+        <mesh
+          geometry={nodes.Object_17.geometry}
+          material={materials.material_0}
+        />
+      </group>
+    </group>
+  );
+}
+
+function App() {
+  return (
+    <Wrapper className="App">
+      <Canvas className="canvas">
+        <OrbitControls enableZoom={false} />
+        <ambientLight intensity={0.5} />
+        <directionalLight position={[-2, 5, 2]} intensity={1} />
+        <Suspense fallback={null}>
+          <Box />
+        </Suspense>
+      </Canvas>
+      <Canvas className="canvas">
+        <OrbitControls enableZoom={false} />
+        <ambientLight intensity={0.5} />
+        <directionalLight position={[-2, 5, 2]} intensity={1} />
+        <Suspense fallback={null}>
+          <AnimatedSphere />
+        </Suspense>
+      </Canvas>
+      <Canvas className="canvas">
+        <OrbitControls enableZoom={false} />
+        <ambientLight intensity={0.5} />
+        <directionalLight position={[-2, 5, 2]} intensity={1} />
+        <Suspense fallback={null}>
+          <Model />
+        </Suspense>
+      </Canvas>
+    </Wrapper>
+  );
+}
+
+export default App;
