3d5ef3f50e8eaefe7a61d3eeaf34d8068973b93c
Sunny-jinn <rlawlsdn316@gmail.com>
2023-03-20 23:32:13 +0900

    refactor: Remove React.Fragment & change structures

--- src/App.js
+++ src/App.js
@@ -91,20 +91,18 @@ function App() {
     //     </Suspense>
     //   </Canvas>
     // </Wrapper>
-    <Canvas>
-      <Suspense fallback={null}>
-        <Environment
-          files={process.env.PUBLIC_URL + "/textures/envmap.hdr"}
-          background={"both"}
-        />
+    <Suspense fallback={null}>
+      <Environment
+        files={process.env.PUBLIC_URL + "/textures/envmap.hdr"}
+        background={"both"}
+      />
 
-        <PerspectiveCamera makeDefault position={[-6, 3.9, 6.21]} fov={40} />
-        <OrbitControls target={[-2.64, -0.71, 0.03]} />
+      <PerspectiveCamera makeDefault position={[-6, 3.9, 6.21]} fov={40} />
+      <OrbitControls target={[-2.64, -0.71, 0.03]} />
 
-        <Track />
-        <Ground />
-      </Suspense>
-    </Canvas>
+      <Track />
+      <Ground />
+    </Suspense>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { Canvas } from "@react-three/fiber";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.Fragment>
+  <Canvas>
     <App />
-  </React.Fragment>
+  </Canvas>
 );
 
 // If you want to start measuring performance in your app, pass a function
