a41327035852ff665b85e54920a4a8cb26c5ce52
Sunny-jinn <rlawlsdn316@gmail.com>
2023-03-27 03:45:53 +0900

    feat: Add third person camera view

--- src/App.js
+++ src/App.js
@@ -1,108 +1,31 @@
-import React, { Suspense, useRef } from "react";
+import React, { Suspense, useEffect, useState } from "react";
 import "./App.css";
-import { Canvas } from "@react-three/fiber";
-import Box from "./Box";
-import styled from "styled-components";
 
 import {
-  useGLTF,
   OrbitControls,
   Environment,
   PerspectiveCamera,
 } from "@react-three/drei";
-import AnimatedSphere from "./AnimatedSphere";
 import { Track } from "./Track";
 import { Ground } from "./Ground";
 import { Car } from "./Car";
 
-const Wrapper = styled.div`
-  position: relative;
-
-  canvas {
-    height: 500px;
-  }
-`;
-function Model(props) {
-  const { nodes, materials } = useGLTF("../scene.gltf");
-  return (
-    <group {...props} dispose={null} scale={0.5}>
-      <group rotation={[-Math.PI / 2, 0, 0]}>
-        <mesh
-          geometry={nodes.Object_3.geometry}
-          material={materials.material_7}
-        />
-        <mesh
-          geometry={nodes.Object_5.geometry}
-          material={materials.material_6}
-        />
-        <mesh
-          geometry={nodes.Object_7.geometry}
-          material={materials.material_5}
-        />
-        <mesh
-          geometry={nodes.Object_9.geometry}
-          material={materials.material_4}
-        />
-        <mesh
-          geometry={nodes.Object_11.geometry}
-          material={materials.material_3}
-        />
-        <mesh
-          geometry={nodes.Object_13.geometry}
-          material={materials.material_2}
-        />
-        <mesh
-          geometry={nodes.Object_15.geometry}
-          material={materials.material_1}
-        />
-        <mesh
-          geometry={nodes.Object_17.geometry}
-          material={materials.material_0}
-        />
-      </group>
-    </group>
-  );
-}
-
 function App() {
+  const [thirdPerson, setThirdPerson] = useState(false);
+  const [cameraPosition, setCameraPosition] = useState([-6, 3.9, 6.21]);
+
   return (
-    // <Wrapper className="App">
-    //   <Canvas className="canvas">
-    //     <OrbitControls enableZoom={false} />
-    //     <ambientLight intensity={0.5} />
-    //     <directionalLight position={[-2, 5, 2]} intensity={1} />
-    //     <Suspense fallback={null}>
-    //       <Box />
-    //     </Suspense>
-    //   </Canvas>
-    //   <Canvas className="canvas">
-    //     <OrbitControls enableZoom={false} />
-    //     <ambientLight intensity={0.5} />
-    //     <directionalLight position={[-2, 5, 2]} intensity={1} />
-    //     <Suspense fallback={null}>
-    //       <AnimatedSphere />
-    //     </Suspense>
-    //   </Canvas>
-    //   <Canvas className="canvas">
-    //     <OrbitControls enableZoom={false} />
-    //     <ambientLight intensity={0.5} />
-    //     <directionalLight position={[-2, 5, 2]} intensity={1} />
-    //     <Suspense fallback={null}>
-    //       <Model />
-    //     </Suspense>
-    //   </Canvas>
-    // </Wrapper>
     <Suspense fallback={null}>
       <Environment
         files={process.env.PUBLIC_URL + "/textures/envmap.hdr"}
         background={"both"}
       />
 
-      <PerspectiveCamera makeDefault position={[-6, 3.9, 6.21]} fov={40} />
-      <OrbitControls target={[-2.64, -0.71, 0.03]} />
-      <Car />
+      <PerspectiveCamera makeDefault position={cameraPosition} fov={40} />
+      {!thirdPerson && <OrbitControls target={[-2.64, -0.71, 0.03]} />}
       <Track />
       <Ground />
+      <Car thirdPerson={thirdPerson} />
     </Suspense>
   );
 }
--- src/Car.jsx
+++ src/Car.jsx
@@ -1,12 +1,13 @@
 import { useBox, useRaycastVehicle } from "@react-three/cannon";
-import { useLoader } from "@react-three/fiber";
+import { useFrame, useLoader } from "@react-three/fiber";
 import { useEffect, useRef } from "react";
+import { Quaternion, Vector3 } from "three";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { useControls } from "./useControls";
 import { useWheels } from "./useWheels";
 import { WheelDebug } from "./WheelDebug";
 
-export function Car() {
+export function Car({ thirdPerson }) {
   let mesh = useLoader(
     GLTFLoader,
     process.env.PUBLIC_URL + "/models/car.glb"
@@ -41,9 +42,31 @@ export function Car() {
 
   useControls(vehicleApi, chassisApi);
 
+  useFrame((state) => {
+    if (!thirdPerson) return;
+
+    let position = new Vector3(0, 0, 0);
+    position.setFromMatrixPosition(chassisBody.current.matrixWorld);
+
+    let quaternion = new Quaternion(0, 0, 0, 0);
+    quaternion.setFromRotationMatrix(chassisBody.current.matrixWorld);
+
+    let wDir = new Vector3(0, 0, 1);
+    wDir.applyQuaternion(quaternion);
+    wDir.normalize();
+
+    let cameraPosition = position
+      .clone()
+      .add(wDir.clone().multiplyScalar(1).add(new Vector3(0, 0.3, 0)));
+
+    wDir.add(new Vector3(0, 0.2, 0));
+    state.camera.position.copy(cameraPosition);
+    state.camera.lookAt(position);
+  });
+
   useEffect(() => {
     mesh.scale.set(0.0012, 0.0012, 0.0012);
-    mesh.children[0].position.set(-265, -18, -67);
+    mesh.children[0].position.set(-365, -18, -67);
   }, [mesh]);
 
   return (
