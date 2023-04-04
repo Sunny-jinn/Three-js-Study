e9e05af33ef8413bc90a094cf3357a1864dd31bd
Sunny-jinn <rlawlsdn316@gmail.com>
2023-04-04 22:44:01 +0900

    feat: Start second project

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Suspense, useEffect, useState } from "react";
+import React, { Suspense, useEffect, useRef, useState } from "react";
 import "./App.css";
 
 import {
@@ -10,27 +10,46 @@ import { Track } from "./Track";
 import { Ground } from "./Ground";
 import { Car } from "./Car";
 import Person from "./Person";
+import { useFrame } from "@react-three/fiber";
 
 function App() {
-  const [thirdPerson, setThirdPerson] = useState(false);
-  const [cameraPosition, setCameraPosition] = useState([-6, 3.9, 6.21]);
+  // const [thirdPerson, setThirdPerson] = useState(false);
+  // const [cameraPosition, setCameraPosition] = useState([-6, 3.9, 6.21]);
 
-  useEffect(() => {
-    function keydownHandler(e) {
-      if (e.key == "k") {
-        if (thirdPerson)
-          setCameraPosition([-6, 3.9, 6.21 + Math.random() * 0.01]);
-        setThirdPerson(!thirdPerson);
-      }
-    }
+  // useEffect(() => {
+  //   function keydownHandler(e) {
+  //     if (e.key == "k") {
+  //       if (thirdPerson)
+  //         setCameraPosition([-6, 3.9, 6.21 + Math.random() * 0.01]);
+  //       setThirdPerson(!thirdPerson);
+  //     }
+  //   }
 
-    window.addEventListener("keydown", keydownHandler);
-    return () => window.removeEventListener("keydown", keydownHandler);
-  }, [thirdPerson]);
+  //   window.addEventListener("keydown", keydownHandler);
+  //   return () => window.removeEventListener("keydown", keydownHandler);
+  // }, [thirdPerson]);
+
+  const box = useRef(null);
+  useFrame(
+    () =>
+      box.current &&
+      void ((box.current.rotation.x += 0.01), (box.current.rotation.y += 0.01))
+  );
 
   return (
     <Suspense fallback={null}>
-      <Environment
+      <mesh ref={box}>
+        <boxGeometry attach="geometry" args={[1, 1, 1]} />
+        <meshLambertMaterial
+          attach={"material"}
+          transparent={true}
+          opacity={1}
+          color={0x000000}
+        />
+        <OrbitControls />
+        {/* <PerspectiveCamera makeDefault position={cameraPosition} fov={40} /> */}
+      </mesh>
+      {/* <Environment
         files={process.env.PUBLIC_URL + "/textures/envmap.hdr"}
         background={"both"}
       />
@@ -39,8 +58,7 @@ function App() {
       {!thirdPerson && <OrbitControls target={[-2.64, -0.71, 0.03]} />}
       <Track />
       <Ground />
-      <Person position={cameraPosition} />
-      {/* <Car thirdPerson={thirdPerson} /> */}
+      <Car thirdPerson={thirdPerson} /> */}
     </Suspense>
   );
 }
