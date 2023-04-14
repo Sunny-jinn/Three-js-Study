7d5498977a770304d4ff3fffb2cf247f04f495b7
Sunny-jinn <rlawlsdn316@gmail.com>
2023-04-15 00:06:44 +0900

    feat: Add character motion

--- src/App.js
+++ src/App.js
@@ -1,32 +1,53 @@
 import React, { Suspense, useEffect, useRef, useState } from "react";
 import "./App.css";
 
-import { OrbitControls, PerspectiveCamera, useHelper } from "@react-three/drei";
-import { useLoader, useThree } from "@react-three/fiber";
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import {
-  Box3,
-  DirectionalLight,
-  DirectionalLightHelper,
-  PointLightHelper,
-} from "three";
+  OrbitControls,
+  PerspectiveCamera,
+  useGLTF,
+  useHelper,
+} from "@react-three/drei";
+import { Canvas, useFrame, useLoader, useThree } from "@react-three/fiber";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { Box3, DirectionalLightHelper, PointLightHelper } from "three";
 import * as THREE from "three";
 
-function App() {
-  let mesh = useLoader(
-    GLTFLoader,
-    process.env.PUBLIC_URL + "/models/character.glb"
-  ).scene;
+const useModelAnimations = (modelRef, gltf) => {
+  const [mixer, setMixer] = useState(null);
+  const animationsMapRef = useRef(new Map());
 
-  const box = useRef(null);
-  const charRef = useRef();
-  const pointRef = useRef();
-  const shadowLightRef = useRef();
-  const { scene } = useThree();
+  const [currentAnimationAction, setCurrentAnimationAction] = useState(null);
+  const [animationsMap, setAnimationsMap] = useState(new Map());
 
-  useHelper(pointRef, PointLightHelper, 10);
-  useHelper(shadowLightRef, DirectionalLightHelper, 10);
-  useHelper(charRef, THREE.BoxHelper);
+  useEffect(() => {
+    if (modelRef.current && gltf && !mixer) {
+      const animationClips = gltf.animations;
+      const mixerInstance = new THREE.AnimationMixer(modelRef.current);
+
+      animationClips.forEach((clip) => {
+        const name = clip.name;
+        console.log(name);
+        animationsMap.set(name, mixerInstance.clipAction(clip));
+      });
+
+      setMixer(mixerInstance);
+      setAnimationsMap(animationsMap);
+      setCurrentAnimationAction(animationsMap.get("Idle"));
+    }
+  }, [modelRef, gltf, mixer]);
+
+  useEffect(() => {
+    if (currentAnimationAction) {
+      currentAnimationAction.play();
+    }
+  }, [currentAnimationAction]);
+
+  return { mixer, animationsMap: animationsMapRef.current };
+};
+
+function Character({ charRef }) {
+  // const modelRef = useRef();
+  const { scene, animations } = useGLTF("/models/character.glb");
 
   useEffect(() => {
     if (charRef.current) {
@@ -38,6 +59,58 @@ function App() {
       charRef.current.position.y = offsetY;
     }
   }, [charRef]);
+  useHelper(charRef, THREE.BoxHelper);
+  console.log("hi");
+
+  const { mixer } = useModelAnimations(charRef, { animations });
+
+  // Update the mixer on each frame
+  useFrame((_, delta) => {
+    if (mixer) {
+      mixer.update(delta);
+    }
+  });
+
+  return (
+    <primitive
+      ref={charRef}
+      object={scene}
+      castShadow={true}
+      // ...other props, e.g., position, rotation, etc.
+    />
+  );
+}
+
+function Ground() {
+  return (
+    <mesh rotation-x={-Math.PI / 2} receiveShadow={true}>
+      <planeGeometry args={[1000, 1000]} />
+      <meshPhongMaterial color={0x878787} />
+    </mesh>
+  );
+}
+
+function App() {
+  const [gltf, setGltf] = useState({});
+  const box = useRef(null);
+  const charRef = useRef();
+  const pointRef = useRef();
+  const shadowLightRef = useRef();
+  const { scene } = useThree();
+
+  // const { scene, animations } = useGLTF("/models/character.glb");
+
+  // const { mixer } = useModelAnimations(charRef, { animations });
+
+  // // Update the mixer on each frame
+  // useFrame((_, delta) => {
+  //   if (mixer) {
+  //     mixer.update(delta);
+  //   }
+  // });
+
+  useHelper(pointRef, PointLightHelper, 10);
+  useHelper(shadowLightRef, DirectionalLightHelper, 10);
 
   useEffect(() => {
     if (shadowLightRef.current) {
@@ -47,75 +120,72 @@ function App() {
   }, [shadowLightRef, scene]);
 
   return (
-    // <Suspense fallback={null}>
-    <mesh ref={box}>
-      {/* <boxGeometry attach="geometry" args={[1, 1, 1]} />
-        <meshLambertMaterial
-          attach={"material"}
-          transparent={true}
-          opacity={1}
-          color={0x244497}
-        /> */}
-      <primitive ref={charRef} object={mesh} castShadow={true} />
-      <mesh rotation-x={-Math.PI / 2} receiveShadow={true}>
-        <planeBufferGeometry args={[1000, 1000]} />
-        <meshPhongMaterial color={0x878787} />
+    <Suspense fallback={null}>
+      <mesh ref={box}>
+        {/* <primitive ref={charRef} object={scene} castShadow={true} /> */}
+        <Ground />
+        <group>
+          <Character charRef={charRef} />
+          {/* <mesh rotation-x={-Math.PI / 2} receiveShadow={true}>
+            <planeGeometry args={[1000, 1000]} />
+            <meshPhongMaterial color={0x878787} />
+          </mesh> */}
+        </group>
+        <PerspectiveCamera
+          makeDefault
+          position={[0, 100, 500]}
+          fov={60}
+          near={1}
+          far={5000}
+        />
+        <ambientLight color={0xffffff} intensity={0.5} />
+        <pointLight
+          ref={pointRef}
+          distance={2000}
+          position={[500, 150, 500]}
+          intensity={1.5}
+          color={0xffffff}
+        />
+        <pointLight
+          ref={pointRef}
+          distance={2000}
+          position={[-500, 150, 500]}
+          intensity={1.5}
+          color={0xffffff}
+        />
+        <pointLight
+          distance={2000}
+          position={[-500, 150, -500]}
+          intensity={1.5}
+          color={0xffffff}
+        />
+        <pointLight
+          distance={2000}
+          position={[500, 150, -500]}
+          intensity={1.5}
+          color={0xffffff}
+        />
+        <directionalLight
+          castShadow={true}
+          ref={shadowLightRef}
+          color={0xffffff}
+          intensity={0.2}
+          position={[200, 500, 200]}
+          target-position={[0, 0, 0]}
+          shadow-mapSize-width={1024}
+          shadow-mapSize-height={1024}
+          shadow-camera-near={100}
+          shadow-camera-far={900}
+          shadow-camera-left={-700}
+          shadow-camera-right={700}
+          shadow-camera-top={700}
+          shadow-camera-bottom={-700}
+          shadow-radius={5}
+        />
+        <axesHelper args={[1000]} />
+        <OrbitControls target={[0, 100, 0]} />
       </mesh>
-      <PerspectiveCamera
-        makeDefault
-        position={[0, 100, 500]}
-        fov={60}
-        near={1}
-        far={5000}
-      />
-      <ambientLight color={0xffffff} intensity={0.5} />
-      <pointLight
-        ref={pointRef}
-        distance={2000}
-        position={[500, 150, 500]}
-        intensity={1.5}
-        color={0xffffff}
-      />
-      <pointLight
-        ref={pointRef}
-        distance={2000}
-        position={[-500, 150, 500]}
-        intensity={1.5}
-        color={0xffffff}
-      />
-      <pointLight
-        distance={2000}
-        position={[-500, 150, -500]}
-        intensity={1.5}
-        color={0xffffff}
-      />
-      <pointLight
-        distance={2000}
-        position={[500, 150, -500]}
-        intensity={1.5}
-        color={0xffffff}
-      />
-      <directionalLight
-        castShadow={true}
-        ref={shadowLightRef}
-        color={0xffffff}
-        intensity={0.2}
-        position={[200, 500, 200]}
-        target-position={[0, 0, 0]}
-        shadow-mapSize-width={1024}
-        shadow-mapSize-height={1024}
-        shadow-camera-near={100}
-        shadow-camera-far={900}
-        shadow-camera-left={-700}
-        shadow-camera-right={700}
-        shadow-camera-top={700}
-        shadow-camera-bottom={-700}
-        shadow-radius={5}
-      />
-      <axesHelper args={[1000]} />
-      <OrbitControls />
-    </mesh>
-    // </Suspense>
+    </Suspense>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -8,18 +8,20 @@ import * as THREE from "three";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Canvas
-    style={{ background: "#000000" }}
-    gl={{ antialias: true }}
-    onCreated={({ gl }) => {
-      gl.shadowMap.enabled = true;
-      gl.shadowMap.type = THREE.VSMShadowMap;
-    }}
-  >
+  <>
     {/* <Physics broadphase="SAP" gravity={[0, -2.6, 0]}> */}
-    <App />
+    <Canvas
+      style={{ background: "#000000" }}
+      gl={{ antialias: true }}
+      onCreated={({ gl }) => {
+        gl.shadowMap.enabled = true;
+        gl.shadowMap.type = THREE.VSMShadowMap;
+      }}
+    >
+      <App />
+    </Canvas>
     {/* </Physics> */}
-  </Canvas>
+  </>
 );
 
 // If you want to start measuring performance in your app, pass a function
