4d248ba1fa7fed7aa1dd7a034b2666ddd4a0ffff
Sunny-jinn <rlawlsdn316@gmail.com>
2023-04-29 03:19:43 +0900

    feat: Add character animation(Rest, Walk)

--- src/AppTwo.js
+++ src/AppTwo.js
@@ -2,23 +2,36 @@ import React, { useEffect, useRef, useState } from "react";
 import { OrbitControls, useGLTF } from "@react-three/drei";
 import { useFrame, useLoader } from "@react-three/fiber";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
-import { MeshStandardMaterial } from "three";
+import * as THREE from "three";
 
 const AppTwo = () => {
   const [controls, setControls] = useState({});
-  //   const { scene, animations } = useGLTF("/models/prevChar.glb");
-  //   const gltf = useLoader(GLTFLoader, "/models/prevChar2.glb");
-  const gltf = useLoader(GLTFLoader, "/models/newChar.glb");
-  //   const material = new MeshStandardMaterial({ color: 0xffffff });
-
-  //   gltf.scene.traverse((child) => {
-  //     if (child.isMesh) {
-  //       child.material = material;
-  //     }
-  //   });
+  const gltf = useLoader(GLTFLoader, "/models/aniChar214.glb");
 
   const boxRef = useRef();
   const charRef = useRef();
+
+  const [mixer] = useState(() => new THREE.AnimationMixer());
+  const animations = gltf.animations;
+
+  // const [animations, setAnimations] = useState(gltf.animations);
+  const [curAnimations, setCurAnimations] = useState(animations[0]);
+
+  useEffect(() => {
+    return () => mixer.stopAllAction(); // Clean up the mixer on unmount
+  }, [mixer]);
+
+  useFrame((state, delta) => {
+    mixer.update(delta); // Update the animation mixer on each frame
+  });
+
+  // Start the desired animation, for example the first one:
+  useEffect(() => {
+    if (!charRef.current) return;
+    const action = mixer.clipAction(curAnimations, charRef.current);
+    mixer.stopAllAction(); // Stop the previous action
+    action.play();
+  }, [curAnimations, mixer]);
 
   useEffect(() => {
     const keyDownPressHandler = (e) => {
@@ -48,6 +61,10 @@ const AppTwo = () => {
       boxRef.current.position.x -= 2;
     } else if (controls.d) {
       boxRef.current.position.x += 2;
+    } else if (controls.w) {
+      setCurAnimations(animations[1]);
+    } else {
+      setCurAnimations(animations[0]);
     }
   });
 
@@ -60,6 +77,9 @@ const AppTwo = () => {
       <mesh scale={40} ref={boxRef}>
         <primitive object={gltf.scene} ref={charRef} />
       </mesh>
+      {/* <mesh rotation-x={-Math.PI / 2} receiveShadow={true}>
+        <planeGeometry args={[1000, 1000]} />
+      </mesh> */}
       <ambientLight color={0xffffff} intensity={1} />
       <pointLight position={[50, 50, 50]} intensity={1.5} />
       <pointLight position={[-50, 70, -50]} intensity={1.5} />
