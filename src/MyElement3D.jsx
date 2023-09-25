345e0c2d7c41e6dc49ec08e5d7a08f8becea76e2
Sunny-jinn <rlawlsdn316@gmail.com>
2023-09-25 23:59:14 +0900

    feat: Functional geometry

--- src/MyElement3D.jsx
+++ src/MyElement3D.jsx
@@ -1,33 +1,36 @@
-import { OrbitControls } from "@react-three/drei";
+import { Box, OrbitControls } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
 import { useRef } from "react";
 import * as THREE from "three";
 
+const MyBox = (props) => {
+  const geom = new THREE.BoxGeometry();
+  return <mesh {...props} geometry={geom}></mesh>;
+};
+
 const MyElement3D = () => {
+  // Drei : R3F에서 사용할 수 있는 유용한 컴포넌트들을 모아놓은 라이브러리
   const refMesh = useRef();
 
   return (
     <>
-      <directionalLight position={[1, 1, 1]} />
+      <ambientLight intensity={0.1} />
+      <directionalLight position={[2, 1, 3]} intensity={0.5} />
 
-      <axesHelper scale={10} />
       <OrbitControls />
 
-      <mesh
-        ref={refMesh}
-        position={[1, 2, 0]}
-        rotation={[0, THREE.MathUtils.degToRad(45), 0]}
-        scale={[2, 1, 1]}
-      >
+      <mesh ref={refMesh}>
         <boxGeometry />
-        <meshStandardMaterial color="#e67e22" opacity={0.5} transparent />
-        <axesHelper />
-
-        <mesh scale={[0.1, 0.1, 0.1]} position-y={2}>
-          <sphereGeometry />
-          <meshStandardMaterial color="red" />
-        </mesh>
+        <meshStandardMaterial color="#1abc9c" opacity={1} />
       </mesh>
+
+      <Box position={[1.2, 0, 0]}>
+        <meshStandardMaterial color="#8e44ad" opacity={1} />
+      </Box>
+
+      <MyBox position={[-1.2, 0, 0]}>
+        <meshStandardMaterial color="#e74c3c" opacity={1} />
+      </MyBox>
     </>
   );
 };
