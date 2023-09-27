08000333a3b8f9696369a93529bc66638e1dbb23
Sunny-jinn <rlawlsdn316@gmail.com>
2023-09-28 02:19:44 +0900

    feat: Use MeshRefractionMaterial & MeshReflectorMaterial component

--- src/MyElement3D.jsx
+++ src/MyElement3D.jsx
@@ -1,14 +1,27 @@
-import { Box, OrbitControls, useTexture } from "@react-three/drei";
-import { useFrame } from "@react-three/fiber";
+import {
+  Box,
+  CubeCamera,
+  MeshReflectorMaterial,
+  MeshRefractionMaterial,
+  OrbitControls,
+  useTexture,
+} from "@react-three/drei";
+import { useFrame, useLoader } from "@react-three/fiber";
 import { useControls } from "leva";
 import { useEffect, useRef } from "react";
 import * as THREE from "three";
+import { RGBELoader } from "three-stdlib";
 
 const MyElement3D = () => {
   // Drei : R3F에서 사용할 수 있는 유용한 컴포넌트들을 모아놓은 라이브러리
   const refMesh = useRef();
   const refWireMesh = useRef();
   const mesh2 = useRef();
+
+  const texture = useLoader(
+    RGBELoader,
+    "https://dl.polyhaven.org/file/ph-assets/HDRIs/hdr/1k/aerodynamics_workshop_1k.hdr"
+  );
 
   // const { xSize, ySize, zSize, xSegments, ySegments, zSegments } = useControls({
   //   xSize: { value: 1, min: 0.1, max: 5, step: 0.01 },
@@ -28,62 +41,57 @@ const MyElement3D = () => {
   //   refWireMesh.current.geometry = refMesh.current.geometry;
   // }, [xSize, ySize, zSize, xSegments, ySegments, zSegments]);
 
-  useEffect(() => {
-    mesh2.current.material = refMesh.current.material;
-  }, []);
-
-  const mapcap = useTexture("./images/mapcap.jpg");
-
-  const {
-    roughness,
-    metalness,
-    clearcoat,
-    clearcoatRoughness,
-    transmission,
-    thickness,
-    ior,
-  } = useControls({
-    roughness: { value: 0, min: 0, max: 1, step: 0.01 },
-    metalness: { value: 0, min: 0, max: 1, step: 0.01 },
-    clearcoat: { value: 0, min: 0, max: 1, step: 0.01 },
-    transmission: { value: 0, min: 0, max: 1, step: 0.01 },
-    thickness: { value: 0, min: 0, max: 10, step: 0.01 },
-    ior: { value: 1.5, min: 0, max: 2.333, step: 0.01 },
-  });
+  // const mapcap = useTexture("./images/mapcap.jpg");
 
   return (
     <>
-      <ambientLight intensity={0.2} />
+      <OrbitControls />
+
+      <ambientLight intensity={0.1} />
       <directionalLight position={[0, 1, 0]} />
       <directionalLight position={[1, 2, 8]} intensity={0.7} />
 
-      <OrbitControls />
+      <CubeCamera resolution={1024} frames={1} envMap={texture}>
+        {(texture) => (
+          <mesh>
+            <dodecahedronGeometry />
+            <MeshRefractionMaterial
+              envMap={texture}
+              toneMapped={false}
+              bounces={2}
+              aberrationStrength={0.03}
+              ior={2.75}
+              fresnel={1}
+              color={0xffffff}
+              fastChroma={true}
+            />
+          </mesh>
+        )}
+      </CubeCamera>
 
-      <mesh ref={refMesh} position={[0.7, 0, 0]}>
-        <torusKnotGeometry args={[0.5, 0.15, 256, 128]} />
-        {/* <meshPhysicalMaterial
-          visible={true}
-          transparent={true}
-          side={THREE.DoubleSide}
-          depthTest={true}
-          depthWrite={true}
-          color={0xffffff}
-          emissive={0x00000}
-          opacity={1}
-          roughness={roughness}
-          metalness={metalness}
-          clearcoat={clearcoat}
-          clearcoatRoughness={clearcoatRoughness}
-          transmission={transmission}
-          thickness={thickness}
-          ior={ior}
-        /> */}
-        <meshToonMaterial />
+      {
+        //FEAT: 바닥에 reflection 주는 코드
+        /* <mesh position={[0, -0.6, 0]} rotation={[-Math.PI / 2, 0, 0]}>
+        <planeGeometry args={[10, 10]} />
+        <MeshReflectorMaterial
+          blur={[300, 100]}
+          resolution={2048}
+          mixBlur={1}
+          mixStrength={30}
+          roughness={1}
+          depthScale={0.7}
+          minDepthThreshold={0.4}
+          maxDepthThreshold={1.4}
+          metalness={0.5}
+          color={0x050505}
+        />
       </mesh>
 
-      <mesh ref={mesh2} position={[-0.7, 0, 0]}>
-        <torusGeometry args={[0.5, 0.2]} />
-      </mesh>
+      <mesh position={[0, 0, 0]}>
+        <boxGeometry />
+        <meshStandardMaterial color={"cyan"} />
+      </mesh> */
+      }
     </>
   );
 };
