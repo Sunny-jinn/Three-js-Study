0dfe9412538ca103b2ea071a10e1041a9f789f0b
Sunny-jinn <rlawlsdn316@gmail.com>
2023-09-28 18:38:34 +0900

    remove: Remove old files

--- src/MyElement3D.jsx
+++ src/MyElement3D.jsx
@@ -3,51 +3,6 @@ import { useEffect, useRef } from "react";
 import * as THREE from "three";
 
 const MyElement3D = () => {
-  const mesh = useRef();
-
-  useEffect(() => {
-    textures.map.repeat.x =
-      textures.displacementMap.repeat.x =
-      textures.aoMap.repeat.x =
-      textures.roughnessMap.repeat.x =
-      textures.metalnessMap.repeat.x =
-      textures.normalMap.repeat.x =
-      textures.alphaMap.repeat.x =
-        4;
-
-    textures.map.wrapS =
-      textures.displacementMap.wrapS =
-      textures.aoMap.wrapS =
-      textures.roughnessMap.wrapS =
-      textures.metalnessMap.wrapS =
-      textures.normalMap.wrapS =
-      textures.alphaMap.wrapS =
-        THREE.MirroredRepeatWrapping;
-
-    textures.map.needsUpdate =
-      textures.displacementMap.needsUpdate =
-      textures.aoMap.needsUpdate =
-      textures.roughnessMap.needsUpdate =
-      textures.metalnessMap.needsUpdate =
-      textures.normalMap.needsUpdate =
-      textures.alphaMap.needsUpdate =
-        true;
-
-    mesh.current.geometry.setAttribute(
-      "uv2",
-      new THREE.BufferAttribute(mesh.current.geometry.attributes.uv.array, 2)
-    );
-  }, []);
-
-  const textures = useTexture({
-    map: "./images/glass/Glass_Window_002_basecolor.jpg",
-    roughnessMap: "./images/glass/Glass_Window_002_roughness.jpg",
-    metalnessMap: "./images/glass/Glass_Window_002_metallic.jpg",
-    normalMap: "./images/glass/Glass_Window_002_normal.jpg",
-    displacementMap: "./images/glass/Glass_Window_002_height.png",
-    aoMap: "./images/glass/Glass_Window_002_ambientOcclusion.jpg",
-    alphaMap: "./images/glass/Glass_Window_002_opacity_.jpg",
-  });
   return (
     <>
       <OrbitControls />
@@ -56,29 +11,9 @@ const MyElement3D = () => {
       <directionalLight position={[0, 1, -8]} intensity={0.5} />
       <directionalLight position={[1, 2, 8]} intensity={0.5} />
 
-      <mesh ref={mesh}>
-        <cylinderGeometry args={[2, 2, 3, 256, 256, true]} />
-        <meshStandardMaterial
-          transparent
-          side={THREE.DoubleSide}
-          map={textures.map}
-          roughness={2}
-          roughnessMap={textures.roughnessMap}
-          roughnessMap-colorSpace={THREE.NoColorSpace}
-          metalness={0.5}
-          metalnessMap={textures.metalnessMap}
-          metalnessMap-colorSpace={THREE.NoColorSpace}
-          normalMap={textures.normalMap}
-          normalMap-colorSpace={THREE.NoColorSpace}
-          normalScale={1}
-          displacementMap={textures.displacementMap}
-          displacementMap-colorSpace={THREE.NoColorSpace}
-          displacementScale={0.2}
-          displacementBias={-0.2}
-          aoMap={textures.aoMap}
-          alphaMap={textures.alphaMap}
-          // alphaToCoverage
-        />
+      <mesh>
+        <boxGeometry />
+        <meshStandardMaterial color={"cyan"} />
       </mesh>
     </>
   );
--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { Canvas } from "@react-three/fiber";
-import AppTwo from "./AppTwo";
-import * as THREE from "three";
-import { Physics } from "@react-three/cannon";
 import AppThree from "./AppThree";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
