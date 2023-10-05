c42dca5b148ed40a824d3f5a2bd043765d6337df
Sunny-jinn <rlawlsdn316@gmail.com>
2023-10-05 18:16:05 +0900

    Feat(client): Make object movement with mouse

--- Sims-client/src/components/Experience.jsx
+++ Sims-client/src/components/Experience.jsx
@@ -8,15 +8,17 @@ import {
 import { AnimatedWoman } from "./AnimatedWoman";
 import { useAtom } from "jotai";
 import { charactersAtom, mapAtom, socket, userAtom } from "./SocketManager";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import * as THREE from "three";
 import { Item } from "./Items";
 import { useThree } from "@react-three/fiber";
 import { useGrid } from "../hooks/useGrid";
 
 export const Experince = () => {
+  const [buildMode, setBuildMode] = useState(true);
   const [characters] = useAtom(charactersAtom);
   const [map] = useAtom(mapAtom);
+  const [items, setItems] = useState(map.items);
   const [onFloor, setOnFloor] = useState(false);
 
   const scene = useThree((state) => state.scene);
@@ -24,17 +26,94 @@ export const Experince = () => {
   useCursor(onFloor);
   const { vector3ToGrid, gridToVector3 } = useGrid();
 
-  const onCharacterMove = (e) => {
-    const character = scene.getObjectByName(`character-${user}`);
-    if (!character) {
+  const onPlaneClick = (e) => {
+    if (!buildMode) {
+      const character = scene.getObjectByName(`character-${user}`);
+      if (!character) {
+        return;
+      }
+      socket.emit(
+        "move",
+        vector3ToGrid(character.position),
+        vector3ToGrid(e.point)
+      );
+    } else {
+      if (draggedItem !== null) {
+        setItems((prev) => {
+          const newItems = [...prev];
+          newItems[draggedItem].gridPosition = vector3ToGrid(e.point);
+          return newItems;
+        });
+        setDraggedItem(null);
+      }
+    }
+  };
+
+  const [draggedItem, setDraggedItem] = useState(null);
+  const [dragPosition, setDragPosition] = useState(null);
+  const [canDrop, setCanDrop] = useState(false);
+
+  useEffect(() => {
+    if (!draggedItem) {
       return;
     }
-    socket.emit(
-      "move",
-      vector3ToGrid(character.position),
-      vector3ToGrid(e.point)
-    );
-  };
+    const item = items[draggedItem];
+    const width =
+      item.rotation === 1 || item.rotation === 3 ? item.size[1] : item.size[0];
+    const height =
+      item.rotation === 1 || item.rotation === 3 ? item.size[0] : item.size[1];
+
+    let droppable = true;
+
+    // 아이템이 맵 범위를 벗어나지 않았는가
+    if (
+      dragPosition[0] < 0 ||
+      dragPosition[0] + width > map.size[0] * map.gridDivision
+    ) {
+      droppable = false;
+    }
+    if (
+      dragPosition[1] < 0 ||
+      dragPosition[1] + height > map.size[1] * map.gridDivision
+    ) {
+      droppable = false;
+    }
+
+    // 아이템이 다른 아이템과 겹치지 않는가
+    if (!item.walkable && !item.wall) {
+      items.forEach((otherItem, idx) => {
+        // ignore self
+        if (idx == draggedItem) {
+          return;
+        }
+
+        // ignore wall & floor
+        if (otherItem.walkable || otherItem.wall) {
+          return;
+        }
+
+        // check item overlap
+        const otherWidth =
+          otherItem.rotation === 1 || otherItem.rotation === 3
+            ? otherItem.size[1]
+            : otherItem.size[0];
+        const otherHeight =
+          otherItem.rotation === 1 || otherItem.rotation === 3
+            ? otherItem.size[0]
+            : otherItem.size[1];
+        if (
+          dragPosition[0] < otherItem.gridPosition[0] + otherWidth &&
+          dragPosition[0] + width > otherItem.gridPosition[0] &&
+          dragPosition[1] < otherItem.gridPosition[1] + otherHeight &&
+          dragPosition[1] + height > otherItem.gridPosition[1]
+        ) {
+          droppable = false;
+        }
+      });
+    }
+
+    setCanDrop(droppable);
+  }, [dragPosition, draggedItem, items]);
 
   return (
     <>
@@ -42,15 +121,35 @@ export const Experince = () => {
       <ambientLight intensity={0.3} />
       <OrbitControls />
 
-      {map.items.map((item, idx) => (
-        <Item key={`${item.name}-${idx}`} item={item} />
+      {(buildMode ? items : map.items).map((item, idx) => (
+        <Item
+          key={`${item.name}-${idx}`}
+          item={item}
+          onClick={() => setDraggedItem((prev) => (prev === null ? idx : prev))}
+          isDragging={draggedItem === idx}
+          dragPosition={dragPosition}
+          canDrop={canDrop}
+        />
       ))}
       <mesh
         rotation-x={-Math.PI / 2}
         position-y={-0.001}
-        onClick={onCharacterMove}
+        onClick={onPlaneClick}
         onPointerEnter={() => setOnFloor(true)}
         onPointerLeave={() => setOnFloor(false)}
+        onPointerMove={(e) => {
+          if (!buildMode) {
+            return;
+          }
+          const newPosition = vector3ToGrid(e.point);
+          if (
+            !dragPosition ||
+            newPosition[0] !== dragPosition[0] ||
+            newPosition[1] !== dragPosition[1]
+          ) {
+            setDragPosition(newPosition);
+          }
+        }}
         position-x={map.size[0] / 2}
         position-z={map.size[1] / 2}
       >
@@ -58,17 +157,18 @@ export const Experince = () => {
         <meshStandardMaterial color={"#f0f0f0"} />
       </mesh>
       <Grid infiniteGrid fadeDistance={50} fadeStrength={5} />
-      {characters.map((character) => (
-        <AnimatedWoman
-          id={character.id}
-          key={character.id}
-          path={character.path}
-          position={gridToVector3(character.position)}
-          hairColor={character.hairColor}
-          topColor={character.topColor}
-          bottomColor={character.bottomColor}
-        />
-      ))}
+      {!buildMode &&
+        characters.map((character) => (
+          <AnimatedWoman
+            id={character.id}
+            key={character.id}
+            path={character.path}
+            position={gridToVector3(character.position)}
+            hairColor={character.hairColor}
+            topColor={character.topColor}
+            bottomColor={character.bottomColor}
+          />
+        ))}
     </>
   );
 };
--- Sims-client/src/components/Items.jsx
+++ Sims-client/src/components/Items.jsx
@@ -3,9 +3,11 @@ import { mapAtom } from "./SocketManager";
 import { useAtom } from "jotai";
 import { useMemo } from "react";
 import { SkeletonUtils } from "three-stdlib";
+import { useGrid } from "../hooks/useGrid";
 
-export const Item = ({ item }) => {
+export const Item = ({ item, onClick, isDragging, dragPosition, canDrop }) => {
   const { name, gridPosition, size, rotation } = item;
+  const { gridToVector3 } = useGrid();
   const [map] = useAtom(mapAtom);
   const { scene } = useGLTF(`models/items/${name}.glb`);
 
@@ -14,14 +16,27 @@ export const Item = ({ item }) => {
   const height = rotation === 1 || rotation === 3 ? size[0] : size[1];
 
   return (
-    <primitive
-      object={clone}
-      position={[
-        width / map.gridDivision / 2 + gridPosition[0] / map.gridDivision,
-        0,
-        height / map.gridDivision / 2 + gridPosition[1] / map.gridDivision,
-      ]}
-      rotation-y={((rotation || 0) * Math.PI) / 2}
-    />
+    <group
+      onClick={onClick}
+      position={gridToVector3(
+        isDragging ? dragPosition || gridPosition : gridPosition,
+        width,
+        height
+      )}
+    >
+      <primitive object={clone} rotation-y={((rotation || 0) * Math.PI) / 2} />
+      {isDragging && (
+        <mesh>
+          <boxGeometry
+            args={[width / map.gridDivision, 0.2, height / map.gridDivision]}
+          />
+          <meshBasicMaterial
+            color={canDrop ? "green" : "red"}
+            opacity={0.3}
+            transparent
+          />
+        </mesh>
+      )}
+    </group>
   );
 };
