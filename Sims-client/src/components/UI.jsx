a9a299aab9dd0de071c3a00b68629570bcf757be
Sunny-jinn <rlawlsdn316@gmail.com>
2023-10-12 19:07:40 +0900

    Feat: Add button to change avatar

--- Sims-client/src/components/UI.jsx
+++ Sims-client/src/components/UI.jsx
@@ -29,6 +29,7 @@ export const UI = () => {
           }}
         />
       )}
+
       <div className="fixed inset-4 flex items-end justify-center pointer-events-none">
         <div className="flex items-center space-x-4 pointer-events-auto">
           {/* BACK */}
@@ -51,6 +52,28 @@ export const UI = () => {
                   strokeLinecap="round"
                   strokeLinejoin="round"
                   d="M9 15L3 9m0 0l6-6M3 9h12a6 6 0 010 12h-3"
+                />
+              </svg>
+            </button>
+          )}
+          {/* AVATAR */}
+          {!buildMode && !shopMode && (
+            <button
+              className="p-4 rounded-full bg-slate-500 text-white drop-shadow-md cursor-pointer hover:bg-slate-800 transition-colors"
+              onClick={() => setAvatarMode(true)}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
                 />
               </svg>
             </button>
